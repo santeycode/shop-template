ada5ea2a850b29a7cf21fbe5811b3ae54c5a8649
Santey Code <santeycode@yandex.ru>
2016-11-11 19:31:16 +0300

    setup watch, less, grunt, npm run

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt){
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: {
+        options: {
+          port: 3000,
+          base: 'public',
+          open: true,
+        }
+      }
+    },
+    less: {
+      dev: {
+        options: {
+          paths: ['css'],
+          compress: false,
+          modifyVars: {
+            version: '<%= pkg.version %>'
+          }
+        },
+        files: {
+          'public/css/main.css': 'less/main.less'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      less: {
+        files: ['less/**/*.less'],
+        tasks: ['less:dev']
+      },
+      html: {
+        files: ['public/{,*/}*.html']
+      }
+    }
+  });
+
+  grunt.registerTask('default', [
+    'less:dev',
+    'connect',
+    'watch'
+  ]);
+
+};module.exports = function(grunt){
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: {
+        options: {
+          port: 3000,
+          base: 'public',
+          open: true,
+        }
+      }
+    },
+    less: {
+      dev: {
+        options: {
+          paths: ['css'],
+          compress: false,
+          modifyVars: {
+            version: '<%= pkg.version %>'
+          }
+        },
+        files: {
+          'public/css/main.css': 'less/main.less'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      less: {
+        files: ['less/**/*.less'],
+        tasks: ['less:dev']
+      },
+      html: {
+        files: ['public/{,*/}*.html']
+      }
+    }
+  });
+
+  grunt.registerTask('default', [
+    'less:dev',
+    'connect',
+    'watch'
+  ]);
+
+};
